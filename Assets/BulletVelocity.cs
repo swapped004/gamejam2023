59b273d466a8834ef49bab79fc8e90a125f50089
Shinchan054 <108150140+Shinchan054@users.noreply.github.com>
2023-01-08 08:32:22 +0600

    level 4 boss done

--- Assets/BulletVelocity.cs
+++ Assets/BulletVelocity.cs
@@ -8,18 +8,28 @@ public class BulletVelocity : MonoBehaviour
     public float speed = 1.5f;
     public Rigidbody2D rb;
     public enemyScript enemy;
+
+    public BossHealth bh;
     void Start()
     {
         GameObject player  = GameObject.FindGameObjectWithTag("Player");
         rb.velocity = transform.right * player.transform.localScale.x* speed;
-        enemy = GameObject.FindGameObjectWithTag("Enemy").GetComponent<enemyScript>();
+        if(GameObject.FindGameObjectWithTag("Enemy"))
+            enemy = GameObject.FindGameObjectWithTag("Enemy").GetComponent<enemyScript>();
+        if(GameObject.FindGameObjectWithTag("Boss"))
+            bh = GameObject.FindGameObjectWithTag("Boss").GetComponent<BossHealth>();
     }
 
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Enemy")
         {
-            enemy.TakeDamage(20);
+            enemy.TakeDamage(10);
+            //Destroy(gameObject);
+        }
+        if (other.gameObject.tag == "Boss")
+        {   
+            bh.TakeDamage(10);
             //Destroy(gameObject);
         }
         if (!(other.gameObject.tag == "Player")) {
